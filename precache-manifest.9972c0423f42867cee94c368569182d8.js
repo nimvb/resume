2ad1564c7d5c405ba16d9ff5c530b722c76fa0a6
Nima <nima.afzal@gmail.com>
2019-01-05 11:21:11 +0330

    Updates

---
+++ precache-manifest.9972c0423f42867cee94c368569182d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "dd935fdcefbd9d7f1950",
+    "url": "/resume/static/js/main.dd935fdc.chunk.js"
+  },
+  {
+    "revision": "f7f1800eb80a477646e6",
+    "url": "/resume/static/js/1.f7f1800e.chunk.js"
+  },
+  {
+    "revision": "dd935fdcefbd9d7f1950",
+    "url": "/resume/static/css/main.fb13ad40.chunk.css"
+  },
+  {
+    "revision": "b0cf9aa6e5c191f694f6482e1da3a019",
+    "url": "/resume/index.html"
+  }
+];
